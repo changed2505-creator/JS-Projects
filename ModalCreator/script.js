13d3ef01fd1150f82157c338d8da97ec299abe06
changed2505-creator <changed2505@gmail.com>
2026-08-01 23:08:33 +0530

    Refactor Modal Creator: Improve script functionality and update HTML doctype

--- ModalCreator/script.js
+++ ModalCreator/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const hider = document.body.querySelectorAll('.hidden');
+const showButton = document.body.querySelectorAll('.show-modal');
+const closeComp = document.body.querySelector('.close-modal');
+const closeOut = document.body.querySelector('.overlay');
+
+showButton.forEach(e => {
+  e.addEventListener('click', () => {
+    hider.forEach(e => {
+      e.classList.remove('hidden');
+    });
+  });
+});
+
+closeComp.addEventListener('click', () => {
+  hider.forEach(e => {
+    e.classList.add('hidden');
+  });
+});
+
+closeOut.addEventListener('click', () => {
+  hider.forEach(e => {
+    e.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    hider.forEach(e => {
+      e.classList.add('hidden');
+    });
+  }
+});
